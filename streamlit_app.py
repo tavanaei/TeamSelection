e3e3e6ea8052e1a20d5b214b2758a123732d3dd2
Amirhossein Tavanaei <a.tavanaei@gmail.com>
2025-09-10 07:33:46 +0000

    created 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,89 @@
 import streamlit as st
+import pandas as pd
+import copy
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+df = pd.read_csv('Soccer Sunday.csv')
+players = list(df['Player'])
+
+class Team:
+    def __init__(self, player_list):
+        self.table = df
+        self.table = self.table[self.table['Player'].isin(player_list)].reset_index(drop=True)
+        #print(len(self.table))
+        if len(self.table) < 14:
+            dummy = [["Ghoast",4,4,4,2,1] for i in range(14-len(self.table))]
+            self.table = pd.concat([self.table, pd.DataFrame(dummy, columns=self.table.columns)], ignore_index=True)
+        self.table = self.table.sample(frac=1).reset_index(drop=True)
+        self.TeamA = []
+        self.TeamB = []
+    def select_goalie(self,A,B):
+        self.table.sort_values(by=['Goalie'], inplace=True, ascending=False)
+        A.append(self.table.iloc[0]['Player'])
+        self.table.drop(self.table.index[0], inplace=True)
+        B.append(self.table.iloc[0]['Player'])
+        self.table.drop(self.table.index[0], inplace=True)
+    def select_defender(self,A,B):
+        self.table.sort_values(by=['Defense'], inplace=True, ascending=False)
+        A.append(self.table.iloc[0]['Player'])
+        self.table.drop(self.table.index[0], inplace=True)
+        B.append(self.table.iloc[0]['Player'])
+        self.table.drop(self.table.index[0], inplace=True)
+    def select_midfielder(self,A,B):
+        self.table.sort_values(by=['Middle'], inplace=True, ascending=False)
+        A.append(self.table.iloc[0]['Player'])
+        self.table.drop(self.table.index[0], inplace=True)
+        B.append(self.table.iloc[0]['Player'])
+        self.table.drop(self.table.index[0], inplace=True)
+    def select_forward(self,A,B):
+        self.table.sort_values(by=['Forward'], inplace=True, ascending=False)
+        A.append(self.table.iloc[0]['Player'])
+        self.table.drop(self.table.index[0], inplace=True)
+        B.append(self.table.iloc[0]['Player'])
+        self.table.drop(self.table.index[0], inplace=True)
+    def select_rest(self,A,B):
+        self.table['overall'] = self.table[['Goalie','Defense','Middle','Forward']].mean(axis=1)
+        self.table.sort_values(by=['overall'], inplace=True, ascending=False)
+        A.append(self.table.iloc[0]['Player'])
+        self.table.drop(self.table.index[0], inplace=True)
+        if len(self.table)>0:
+            B.append(self.table.iloc[0]['Player'])
+            self.table.drop(self.table.index[0], inplace=True)
+    def make_teams(self):
+        self.select_goalie(self.TeamA,self.TeamB)
+        self.select_forward(self.TeamB,self.TeamA)
+        self.select_midfielder(self.TeamA,self.TeamB)
+        self.select_defender(self.TeamB,self.TeamA)
+        self.select_forward(self.TeamA,self.TeamB)
+        self.select_midfielder(self.TeamB,self.TeamA)
+        self.select_defender(self.TeamA,self.TeamB)
+        while len(self.table)>0:
+            self.select_rest(self.TeamB,self.TeamA)
+
+def select_teams(player_list):
+    myteam = Team(player_list)
+    myteam.make_teams()
+    if len(myteam.TeamA) < len(myteam.TeamB):
+        myteam.TeamA.append(" ")
+    elif len(myteam.TeamB) < len(myteam.TeamA):
+        myteam.TeamB.append(" ")
+
+    return "Captains: "+myteam.TeamA[1]+ " and "+ myteam.TeamB[1],pd.DataFrame({"White-Jersey Team": myteam.TeamA, "Black-Jersey Team": myteam.TeamB})
+
+
+st.title("Sunday Soccer Team Selection")
+teams = None
+with st.sidebar:
+    st.header("Players")
+    
+    selected_options = st.multiselect(
+        "Select attending players:",
+        players
+    )
+    if st.button("Run"):
+        caps,teams = select_teams(selected_options)
+
+if teams is not None:
+    st.write("## The number of Players: {}".format(len(selected_options)))
+    st.write("### {}".format(caps))
+    st.write("-----")
+    st.write(teams)
